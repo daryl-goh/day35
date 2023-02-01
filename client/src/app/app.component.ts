c82a015ae143b3086dffedbd9e97f3859c2c19fb
daryl-goh <goh.daryl@gmail.com>
2023-02-01 19:04:09 +0800

    ViewChild Programatically Binding Property and Events

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,9 +10,35 @@ import { BGGService } from './bgg.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnDestroy {
+
+  @ViewChild(SearchComponent)
+  searchComp!: SearchComponent
+
+  @ViewChild(DisplayComponent)
+  displayComp!: DisplayComponent
 
   games: Game[] = []
+
+  onResults$!: Subscription
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    // manually subscribe to the onResults event - @Output
+    this.onResults$ = this.searchComp.onResults.subscribe(
+      (games: Game[]) => {
+        console.info('>>>> in subscription')
+        // manually assign the new value to the attribute - @Input()
+        this.displayComp.games = games
+      }
+    )
+  }
+
+  ngOnDestroy(): void {
+      this.onResults$.unsubscribe()
+  }
+
 
   onResults(games: Game[]) {
     this.games = games
